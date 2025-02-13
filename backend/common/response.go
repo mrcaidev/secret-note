7fb948b7213da1000c999aaa7b5e8037ac1fff95
Guohezu <guohezu@tju.edu.cn>
2025-02-13 22:12:33 +0800

    finish otp

--- backend/common/response.go
+++ backend/common/response.go
@@ -1,5 +1,7 @@
 package common
 
+import "strconv"
+
 type Response struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
@@ -11,5 +13,26 @@ const (
 	EmailExists             = 1001
 	WrongOtp                = 1002
 	WrongOtpFlowIdOrExpired = 1003
+	SendMailFailed          = 1004
 	Success                 = 0
+	Error                   = -1
 )
+
+func ErrCodeToString(code int) string {
+	switch code {
+	case EmailExists:
+		return "EmailExists"
+	case WrongOtp:
+		return "WrongOtp"
+	case WrongOtpFlowIdOrExpired:
+		return "WrongOtpFlowIdOrExpired"
+	case SendMailFailed:
+		return "SendMailFailed"
+	case Success:
+		return "Success"
+	case Error:
+		return "Error"
+	default:
+		return strconv.Itoa(code)
+	}
+}
--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"backend/common"
 	"backend/models"
 	"backend/services"
 	"github.com/gin-gonic/gin"
@@ -18,9 +19,20 @@ func SendOtp(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	otp, err := services.SendOtp(email.Email)
+	otpFlowId, err := services.SendOtp(email.Email)
 	if err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		response := common.Response{
+			Code:    common.SendMailFailed,
+			Message: err.Error(),
+		}
+		c.JSON(http.StatusOK, response)
+	} else {
+		response := common.Response{
+			Code:    common.Success,
+			Message: "otp flow successfully",
+			Data:    otpFlowId,
+		}
+		c.JSON(http.StatusOK, response)
 	}
 }
 
@@ -30,5 +42,11 @@ func VerifyOtp(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	services.VerifyOtp(otpFlow)
+	ret := services.VerifyOtp(otpFlow)
+	response := common.Response{
+		Code:    ret,
+		Message: common.ErrCodeToString(ret),
+		Data:    ret,
+	}
+	c.JSON(http.StatusOK, response)
 }
