bd9d753f48d1d68cc72c279717dff1eeecc3a5a3
Yuwang Cai <mrcaidev@gmail.com>
2025-02-14 19:41:42 +0800

    refactor: no need to customize status bar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,10 @@
 import { FontLoader } from "@/components/font-loader";
 import { QueryProvider } from "@/components/query-provider";
-import { StatusBar } from "@/components/status-bar";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/global.css";
 import { PortalHost } from "@rn-primitives/portal";
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
