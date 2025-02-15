d93108f70bb14d7b05751e9e867616ae99566190
Yuwang Cai <mrcaidev@gmail.com>
2025-02-15 16:19:55 +0800

    fix: missing privacy page content

--- frontend/app/privacy.tsx
+++ frontend/app/privacy.tsx
@@ -1,5 +1,28 @@
-import { Text } from "@/components/ui/text";
+import { H1, P } from "@/components/ui/typography";
+import { View } from "react-native";
 
 export default function PrivacyPolicyPage() {
-  return <Text>Privacy policy</Text>;
+  return (
+    <View className="grow px-6 py-24 bg-background">
+      <H1 className="mb-6">Privacy Policy</H1>
+      <P className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi vitae
+        veritatis, nostrum libero voluptatem non ipsam? Porro aliquid obcaecati
+        quam dicta veritatis, illum id vel, earum voluptatum odit temporibus
+        omnis.
+      </P>
+      <P className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores totam
+        asperiores eveniet iusto eligendi. Nam, aspernatur saepe, facere id
+        assumenda quidem doloremque cumque fugit sapiente ducimus error suscipit
+        veniam non?
+      </P>
+      <P className="mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+        reiciendis exercitationem, ipsa doloribus, unde expedita numquam odit
+        dicta eaque vero molestiae dolores, ut odio labore. Vero minus neque
+        consequuntur perferendis?
+      </P>
+    </View>
+  );
 }
