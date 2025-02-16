dcc075fa8f1b4413c87e93176c265ed66396ee2e
Yuwang Cai <mrcaidev@gmail.com>
2025-02-16 16:59:34 +0800

    refactor: style

--- frontend/app/notes/[id].tsx
+++ frontend/app/notes/[id].tsx
@@ -1,12 +1,13 @@
 import { useNoteQuery } from "@/apis/note";
 import { Avatar } from "@/components/avatar";
-import { FullscreenLoading } from "@/components/fullscreen-loading";
+import { Button } from "@/components/ui/button";
 import { Icon } from "@/components/ui/icon";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Text } from "@/components/ui/text";
 import { H1, Muted, P } from "@/components/ui/typography";
 import { Link, useLocalSearchParams } from "expo-router";
-import { ArrowLeftIcon, PenLineIcon } from "lucide-react-native";
-import { View } from "react-native";
+import { ArrowLeftIcon, HouseIcon, PenLineIcon } from "lucide-react-native";
+import { Pressable, View } from "react-native";
 
 const dateTimeFormat = new Intl.DateTimeFormat("en", {
   dateStyle: "medium",
@@ -19,23 +20,16 @@ export default function NotePage() {
   const { data: note, error, isPending } = useNoteQuery(id);
 
   if (isPending) {
-    return <FullscreenLoading />;
+    return <LoadingScreen />;
   }
 
   if (error) {
-    return (
-      <View className="grow justify-center items-center">
-        <Text>Error: {error.message}</Text>
-      </View>
-    );
+    return <NotFoundScreen />;
   }
 
   return (
-    <View className="px-8 py-16">
-      <Link href="/" className="flex flex-row items-center gap-2 py-5">
-        <Icon as={ArrowLeftIcon} size={18} className="text-muted-foreground" />
-        <Text className="text-muted-foreground">Home</Text>
-      </Link>
+    <View className="px-8 pt-16 bg-background">
+      <HomeLink />
       <H1 className="mb-6">{note.title}</H1>
       <View className="flex-row items-center gap-3 mb-6">
         <Avatar user={note.author} className="size-8" />
@@ -43,15 +37,62 @@ export default function NotePage() {
           {note.author.nickname} shared with me
         </Text>
       </View>
-      {note.content.split("\n").map((line) => (
-        <P key={line}>{line}</P>
+      {note.content.split("\n").map((paragraph, index) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: immutable
+        <P key={index} className="mb-4">
+          {paragraph}
+        </P>
       ))}
-      <View className="flex-row items-center gap-2 mt-6">
+      <View className="flex-row items-center gap-2 mt-2">
         <Icon as={PenLineIcon} className="text-muted-foreground" />
         <Muted>
           Created on {dateTimeFormat.format(new Date(note.createdAt))}
         </Muted>
       </View>
     </View>
+  );
+}
+
+function LoadingScreen() {
+  return (
+    <View className="grow px-8 pt-16 bg-background">
+      <HomeLink />
+      <Skeleton className="h-10 mb-6" />
+      <View className="flex-row items-center gap-3 mb-6">
+        <Skeleton className="size-8 rounded-full" />
+        <Skeleton className="grow h-8" />
+      </View>
+      <Skeleton className="grow mb-6" />
+      <Skeleton className="w-2/3 h-5 mb-6" />
+    </View>
+  );
+}
+
+function NotFoundScreen() {
+  return (
+    <View className="grow justify-center items-center px-8 bg-background">
+      <Text className="mb-2 text-[112px] font-bold">404</Text>
+      <Text className="mb-3 text-2xl font-medium">Oops! Note not found</Text>
+      <Text className="mb-6 text-muted-foreground text-center text-balance">
+        This note no longer exists... Or maybe it never did.
+      </Text>
+      <Link href="/" asChild>
+        <Button>
+          <Icon as={HouseIcon} />
+          <Text>Take me home</Text>
+        </Button>
+      </Link>
+    </View>
+  );
+}
+
+function HomeLink() {
+  return (
+    <Link href="/" asChild>
+      <Pressable className="flex-row items-center gap-2 w-fit py-5">
+        <Icon as={ArrowLeftIcon} className="text-muted-foreground" />
+        <Text className="text-muted-foreground">Home</Text>
+      </Pressable>
+    </Link>
   );
 }
