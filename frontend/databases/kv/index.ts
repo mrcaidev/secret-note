1fb0c7596d0fe45d9d9b1ce9128b06b3a12dae3b
Yuwang Cai <mrcaidev@gmail.com>
2025-03-03 12:10:34 +0800

    feat: use sqlite-cipher

--- frontend/databases/kv/index.ts
+++ frontend/databases/kv/index.ts
@@ -4,3 +4,8 @@ export const tokenDb = new KvDb<string>("token", {
   secure: true,
   session: false,
 });
+
+export const sqliteEncryptionKeyDb = new KvDb<string>("sqlite-encryption-key", {
+  secure: true,
+  session: false,
+});
--- frontend/databases/relational/db.native.ts
+++ frontend/databases/relational/db.native.ts
@@ -1,23 +1,50 @@
 import { devLog } from "@/utils/dev";
+import * as Crypto from "expo-crypto";
 import * as Sqlite from "expo-sqlite";
+import { sqliteEncryptionKeyDb } from "../kv";
 
-devLog("rdb: sqlite initializing");
+async function getEncryptionKey() {
+  const key = await sqliteEncryptionKeyDb.get();
 
+  if (key) {
+    return key;
+  }
+
+  devLog("rdb: no sqlite encryption key, creating one");
+  const newKey = Crypto.randomUUID();
+  sqliteEncryptionKeyDb.set(newKey);
+  return newKey;
+}
+
+devLog("rdb: opening sqlite");
 export const rdb = Sqlite.openDatabaseSync("secretNote");
+devLog("rdb: opened sqlite");
 
-devLog("rdb: sqlite initialized, bootstrapping");
+async function bootstrap() {
+  devLog("rdb: bootstrapping sqlite");
 
-rdb.runSync(
-  `create table if not exists notes (
-    id text primary key,
-    title text not null,
-    content text not null,
-    author_id text not null,
-    author_nickname text not null,
-    author_avatar_url text not null,
-    link text not null,
-    created_at text not null
-  )`,
-);
+  const encryptionKey = await getEncryptionKey();
+  devLog(`rdb: encryption key: ${encryptionKey}`);
 
-devLog("rdb: sqlite bootstrapped");
+  rdb.runAsync(
+    `
+    pragma journal_mode = WAL;
+    pragma key = ?;
+    create table if not exists notes (
+      id text primary key,
+      title text not null,
+      content text not null,
+      author_id text not null,
+      author_nickname text not null,
+      author_avatar_url text not null,
+      link text not null,
+      created_at text not null
+    );
+    `,
+    encryptionKey,
+  );
+
+  devLog("rdb: bootstrapped sqlite");
+}
+
+bootstrap();
