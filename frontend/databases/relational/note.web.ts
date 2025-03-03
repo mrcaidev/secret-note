1f8471360ea86a6b16b06eabc3d3b2ce900875d1
Yuwang Cai <mrcaidev@gmail.com>
2025-03-03 11:23:39 +0800

    fix: still provide functions even not supported

--- frontend/databases/relational/note.web.ts
+++ frontend/databases/relational/note.web.ts
@@ -1,3 +1,17 @@
-throw new Error(
-  "noteDb: web not supported. this file should have been imported in the first place",
-);
+export function findAll() {
+  throw new Error(
+    "noteDb: web not supported. this file should have been imported in the first place",
+  );
+}
+
+export function findOneById() {
+  throw new Error(
+    "noteDb: web not supported. this file should have been imported in the first place",
+  );
+}
+
+export function insertOne() {
+  throw new Error(
+    "noteDb: web not supported. this file should have been imported in the first place",
+  );
+}
